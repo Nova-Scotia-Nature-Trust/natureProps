a58228d9542e231868bde1d71d0a3189cb38c144
DomHenry <dominichenry@gmail.com>
2026-09-17 14:53:05 -0300

    Bug fix to prevent submitting blank contact ID

--- R/module_add_property_contact_communication.R
+++ R/module_add_property_contact_communication.R
@@ -293,7 +293,7 @@ module_add_property_contact_communication_server <- function(
         req(input$contact, iv_contact$is_valid())
         # Create the new communication record
         new_communication <- tibble(
-          property_contact_id = input$contact,
+          property_contact_id = input$contact[input$contact != ""],
           property_id = input$contact_property_id,
           communication_purpose_id = input$communication_purpose_id,
           communication_method_id = input$communication_method_id,
